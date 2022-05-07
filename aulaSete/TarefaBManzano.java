dd848cc08d609fa1db9d0ac68f3b355554ccc901
cgviak <104108988+cgviak@users.noreply.github.com>
2022-05-06 22:21:13 -0300

    Update TarefaBManzano.java

--- aulaSete/TarefaBManzano.java
+++ aulaSete/TarefaBManzano.java
@@ -3,11 +3,12 @@ import java.util.*;
 public class TarefaBManzano {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner (System.in);
-		int a = 0;
+		int a = 0, b = 0;
 		while (a < 100) {
 			System.out.println(a+1);
 			a++;
+			b += a;
 	}
-		System.out.println("= 5050");
+		System.out.println(b);
 	}
 }
