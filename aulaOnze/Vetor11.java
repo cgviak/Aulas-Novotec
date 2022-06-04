b93061d33ae6075c8e56a0bdcc3ef8fc8d8836c3
cgviak <104108988+cgviak@users.noreply.github.com>
2022-06-03 22:28:43 -0300

    Add files via upload

---
+++ aulaOnze/Vetor11.java
@@ -0,0 +1,15 @@
+package aulaOnze;
+
+public class Vetor11 {
+
+	public static void main(String[] args) {
+		
+
+	int [] c = {30, -30, 3, -11, 1, -73, 49, -29, 30, -90, -50};
+		for (int a : c) {
+			if (a < 0) {
+				System.out.print(a + " ");
+			} 
+		}
+	}
+}
