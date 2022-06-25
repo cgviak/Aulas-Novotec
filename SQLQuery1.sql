c23a291a3ee2076521e1de6aa4031e3df7488caf
cgviak <104108988+cgviak@users.noreply.github.com>
2022-06-24 22:23:27 -0300

    Add files via upload

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,23 +1,121 @@
-create database Bibliotecavirtual
-Use Bibliotecavirtual
-CREATE TABLE Categorias (
-ID_categoria int primary key identity,
-nome varchar (80) not null);
-CREATE TABLE Autores (
-id_autor int primary key identity,
-nome varchar (120) not null,
-ano_nascimento smallint null);
-CREATE TABLE Livros (
-Id_livro int primary key identity,
-nome varchar (120) not null,
-numero_pags smallint not null,
-sinopse varchar(max) null,
-ID_Categoria int not null foreign key REFERENCES Categorias(ID_categoria),
-id_autor int not null foreign key REFERENCES Autores(id_autor))
-CREATE TABLE teste (
-codigo int);
-ALTER TABLE Livros
-ADD ano_pub smallint;
-DROP TABLE teste;
+SELECT *
+FROM tipoCartao
 
+SELECT *
+FROM Clientes
+WHERE Cidade = 'São Paulo'
 
+SELECT *
+FROM Clientes
+WHERE DDD = 19
+
+SELECT *
+FROM Clientes
+WHERE Cidade <> 'Valinhos'
+
+SELECT *
+FROM Agencias
+WHERE DDD = 19 AND Cidade = 'Campinas'
+
+SELECT *
+FROM logTransferencias
+WHERE Aprovado = 1 AND idTipoTransferencia = 2 AND Valor >= 2900
+
+SELECT *
+FROM logTransferencias
+WHERE idContaCreditada IN (1701, 1999, 911, 705)
+
+SELECT *
+FROM logTransferencias
+WHERE Valor BETWEEN 1000 and 1500
+
+SELECT *
+FROM Funcionarios
+WHERE Nome LIKE 'Al%'
+
+SELECT *
+FROM logTransferencias
+ORDER BY Valor DESC
+
+SELECT TOP 10 *
+FROM logTransferencias
+ORDER BY Valor DESC
+
+-- exercícios
+
+SELECT *
+FROM tipoCartao
+
+SELECT *
+FROM Bandeiras
+
+SELECT Nome, DDD, Telefone, Email
+FROM Clientes
+
+SELECT idAgencia, Endereco, Cidade, Estado
+FROM Agencias
+
+SELECT *
+FROM Clientes
+WHERE Cidade = 'Valinhos'
+
+SELECT *
+FROM Contas
+WHERE dataAbertura = '2011-10-13'
+
+SELECT *
+FROM Clientes
+WHERE Renda >= 2000
+
+SELECT *
+FROM Clientes
+WHERE Cidade = 'São Paulo' AND rendaComprovada = 1
+
+SELECT *
+FROM Contas
+WHERE talaoCheque = 1 OR internetBanking = 0
+
+SELECT *
+FROM Cliente_Cartao
+WHERE dataPedido BETWEEN '2011-10-01' AND '2011-10-30'
+
+SELECT *
+FROM Contas
+WHERE idConta BETWEEN 10 AND 20
+
+SELECT *
+FROM Contas
+WHERE idConta IN (1, 7, 9, 15, 20)
+
+SELECT *
+FROM Clientes
+WHERE Nome LIKE 'Ana%'
+
+SELECT *
+FROM Clientes
+WHERE Nome LIKE '%ao'
+
+SELECT *
+FROM Funcionarios
+WHERE Nome LIKE '%el%'
+
+SELECT *
+FROM Contas
+ORDER BY Limite ASC
+
+SELECT *
+FROM Bandeiras
+ORDER BY idBandeira DESC
+
+SELECT TOP 20 percent Renda
+FROM Clientes
+
+SELECT *
+FROM tipoCartao
+WHERE rendaMinima >= 2000 AND Internacional = 1
+ORDER BY Anuidade DESC
+
+SELECT TOP 10 *
+FROM Cliente_Cartao
+WHERE Bloqueado = 1
+ORDER BY Numero DESC
